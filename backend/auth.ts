4ae42ec343825edb123dfd9eb222ccd1b30b6de9
Osvaldo Prajitno <z5473276@ad.unsw.edu.au>
2024-10-16 22:23:04 +1100

    implemented function to authenticate student by zID

---
+++ backend/auth.ts
@@ -0,0 +1,36 @@
+
+async function authenticateStudent(zID: string, password: string): Promise<boolean> {
+    if (process.env.NODE_ENV !== 'development') {
+        if (/^[a-zA-Z0-9]+$/.test(zID) || !/^z[0-9]{7}$/.test(zID)) {
+
+        const payload = { zid: zID, zpass: password };
+        const verifyResponse = await fetch('https://verify.csesoc.unsw.edu.au/v1', {
+            method: 'POST',
+            headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        });
+
+        if (verifyResponse.ok) {
+            console.log(`STUDENT=${zID} is logged in`);
+            return true;
+        }
+
+        if (verifyResponse.status === 401) {
+            console.error(`Failed to login STUDENT=${zID} due to INCORRECT PASSWORD`);
+        } else {
+            console.error(
+            `Failed to login STUDENT=${zID} due to ERROR CODE ${verifyResponse.status}`,
+            );
+        }
+        return false;
+        }
+
+        // if unexpected characters are found, immediately reject
+        console.error(`Failed to login STUDENT=${zID} due to INVALID FORMAT`);
+        return false;
+    }
+    return true;
+}
