fb5397175e7760390f5c3619808cc25fe52b8064
Tet Nay Lin <tetnaylin168@gmail.com>
2024-10-17 23:04:56 +1100

    Add optional Year of Study to SignUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
 import SignUp from './components/sign-up/SignUp'
+import SocietySignUp from "./components/society-sign-up/SocietySignUp"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         {/* <Route path="/" element={<Landing />} /> */}
         {/* to change to "/sign-up" afterwards */}
         <Route path="/" element={<SignUp />} />
+        <Route path="/society-sign-up" element={<SocietySignUp />} />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/components/society-sign-up/SocietySignUp.jsx
+++ frontend/src/components/society-sign-up/SocietySignUp.jsx
@@ -9,7 +9,7 @@ import FormControl from '@mui/material/FormControl';
 import Stack from '@mui/material/Stack';
 import MuiCard from '@mui/material/Card';
 import Input from '@mui/material/Input';
-import { styled } from '@mui/material/styles';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import ProfileImage from '../../assets/logo.png';
 import { Avatar, Typography } from '@mui/material';
 
@@ -41,21 +41,18 @@ const SignUpContainer = styled(Stack)(({ theme }) => ({
   },
 }));
 
-export default function SignUp() {
+export default function SocietySignUp() {
   const [nameError, setNameError] = React.useState(false);
   const [nameErrorMessage, setNameErrorMessage] = React.useState('');
   const [emailError, setEmailError] = React.useState(false);
   const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
   const [zIdError, setzIdError] = React.useState(false);
   const [zIdErrorMessage, setzIdErrorMessage] = React.useState('');
-  const [yearError, setYearError] = React.useState(false);
-  const [yearErrorMessage, setYearErrorMessage] = React.useState('');
 
   const validateInputs = () => {
     const email = document.getElementById('email');
     const zId = document.getElementById('zId');
     const name = document.getElementById('name');
-    const year = document.getElementById('year');
 
     let isValid = true;
 
@@ -77,20 +74,11 @@ export default function SignUp() {
       setNameErrorMessage('');
     }
 
-    if (year.value < 0 || year.value > 20) {
-      setYearError(true);
-      setYearErrorMessage('Please a valid year of study.');
-      isValid = false;
-    } else {
-      setYearError(false);
-      setYearErrorMessage('');
-    }
-
     return isValid;
   };
 
   const handleSubmit = (event) => {
-    if (nameError || emailError || zIdError || yearError) {
+    if (nameError || emailError || zIdError) {
       event.preventDefault();
       return;
     }
@@ -100,7 +88,6 @@ export default function SignUp() {
       lastName: data.get('lastName'),
       email: data.get('email'),
       zId: data.get('zId'),
-      year: data.get('year')
     });
   };
 
@@ -181,22 +168,6 @@ export default function SignUp() {
               id="name"
               placeholder="itz_potato"
               color={nameError ? 'red' : 'secondary'}
-              sx={{"&:before": { borderColor: "primary.main" }}} />
-          </FormControl>
-          <FormControl>
-            <FormLabel
-              htmlFor="year"
-              sx={{ display: 'flex', alignSelf: 'flex-start' }}
-            >Year of Study</FormLabel>
-            <Input
-              autoComplete="year"
-              name="year"
-              fullWidth
-              id="year"
-              placeholder="1, 2, 3, ..."
-              error={yearError}
-              helperText={yearErrorMessage}
-              color={yearError ? 'red' : 'secondary'}
               sx={{"&:before": { borderColor: "primary.main" }}} />
           </FormControl>
           <Box
