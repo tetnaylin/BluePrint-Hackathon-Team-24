751080851d3ef35fd9b99197c4cb9880c794fa7a
Francesco Cliff Hervenlee <z5491631@ad.unsw.edu.au>
2024-10-18 04:53:36 +1100

    finsished qrcodescanned

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
 import SignUp from './components/sign-up/SignUp'
+import QrCodeScanned from './components/qr-code-scanned/QrCodeScanned'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         {/* <Route path="/" element={<Landing />} /> */}
         {/* to change to "/sign-up" afterwards */}
         <Route path="/" element={<SignUp />} />
+        <Route path="/scanned" element={<QrCodeScanned />} />
       </Routes>
     </BrowserRouter>
   )
