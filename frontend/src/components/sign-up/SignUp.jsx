f8cd1a0c49279a89661fd74f1feb2f5b6dc37085
Tet Nay Lin <tetnaylin168@gmail.com>
2024-10-17 17:24:41 +1100

    Start of SignUp page

---
+++ frontend/src/components/sign-up/SignUp.jsx
@@ -0,0 +1,244 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import MuiCard from '@mui/material/Card';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import getSignUpTheme from './theme/getSignUpTheme';
+import ProfileImage from '../../assets/logo.png';
+import { Avatar } from '@mui/material';
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: 'auto',
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  [theme.breakpoints.up('sm')]: {
+    width: '450px',
+  },
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
+
+const SignUpContainer = styled(Stack)(({ theme }) => ({
+  minHeight: '100%',
+  padding: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(0),
+  },
+  backgroundImage:
+    'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+  backgroundRepeat: 'no-repeat',
+  ...theme.applyStyles('dark', {
+    backgroundImage:
+      'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+  }),
+}));
+
+export default function SignUp() {
+  const [mode, setMode] = React.useState('dark');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const defaultTheme = createTheme({ palette: { mode } });
+  const SignUpTheme = createTheme(getSignUpTheme(mode));
+  const [nameError, setNameError] = React.useState(false);
+  const [nameErrorMessage, setNameErrorMessage] = React.useState('');
+  const [emailError, setEmailError] = React.useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
+  // This code only runs on the client side, to determine the system color preference
+  React.useEffect(() => {
+    // Check if there is a preferred mode in localStorage
+    const savedMode = localStorage.getItem('themeMode');
+    if (savedMode) {
+      setMode(savedMode);
+    } else {
+      // If no preference is found, it uses system preference
+      const systemPrefersDark = window.matchMedia(
+        '(prefers-color-scheme: dark)',
+      ).matches;
+      setMode(systemPrefersDark ? 'dark' : 'light');
+    }
+  }, []);
+
+  const validateInputs = () => {
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const name = document.getElementById('name');
+
+    let isValid = true;
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true);
+      setEmailErrorMessage('Please enter a valid email address.');
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
+    }
+
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage('Password must be at least 6 characters long.');
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage('');
+    }
+
+    if (!name.value || name.value.length < 1) {
+      setNameError(true);
+      setNameErrorMessage('Name is required.');
+      isValid = false;
+    } else {
+      setNameError(false);
+      setNameErrorMessage('');
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = (event) => {
+    if (nameError || emailError || passwordError) {
+      event.preventDefault();
+      return;
+    }
+    const data = new FormData(event.currentTarget);
+    console.log({
+      name: data.get('name'),
+      lastName: data.get('lastName'),
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={showCustomTheme ? SignUpTheme : defaultTheme}>
+      <CssBaseline enableColorScheme />
+      <SignUpContainer direction="column" justifyContent="space-between">
+        <Card variant="outlined">
+          <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
+            <Avatar
+              alt="Profile Image"
+              src={ProfileImage}
+              sx={{ width: 150, height: 150 }}
+            />
+          </Box>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+          >
+            <FormControl>
+              <FormLabel 
+                htmlFor="name"
+                sx={{ display: 'flex', alignSelf: 'flex-start' }}
+              >* Full name</FormLabel>
+              <TextField
+                autoComplete="name"
+                name="name"
+                required
+                fullWidth
+                id="name"
+                placeholder="Supreme Potato"
+                error={nameError}
+                helperText={nameErrorMessage}
+                color={nameError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel 
+                htmlFor="email"
+                sx={{ display: 'flex', alignSelf: 'flex-start' }}
+              >* Email</FormLabel>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                placeholder="potatoiskool@email.com"
+                name="email"
+                autoComplete="email"
+                variant="outlined"
+                error={emailError}
+                helperText={emailErrorMessage}
+                color={passwordError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel 
+                htmlFor="password"
+                sx={{ display: 'flex', alignSelf: 'flex-start' }}
+              >* zId</FormLabel>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                placeholder="z1234567"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                variant="outlined"
+                error={passwordError}
+                helperText={passwordErrorMessage}
+                color={passwordError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel 
+                htmlFor="name"
+                sx={{ display: 'flex', alignSelf: 'flex-start' }}
+              >Discord</FormLabel>
+              <TextField
+                autoComplete="name"
+                name="name"
+                fullWidth
+                id="name"
+                placeholder="itz_potato"
+                color={nameError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <Box
+              alignItems="center"
+              pb= '15%'
+            >
+              <FormControlLabel 
+                control={<Checkbox value="allowExtraEmails" color="primary"/>}
+                label="Arc Member?"
+              />
+            </Box>
+            
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: 10, justifyContent: 'center' }}>
+              <Button
+                type="submit"
+                variant="contained"
+                onClick={validateInputs}
+              >
+                Sign up
+              </Button>
+              <Button
+                type="cancel"
+                variant="contained"
+                onClick={validateInputs}
+              >
+                Cancel
+              </Button>
+            </Box>
+          </Box>
+        </Card>
+      </SignUpContainer>
+    </ThemeProvider>
+  );
+}
