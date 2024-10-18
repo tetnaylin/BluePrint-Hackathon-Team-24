54b10f266033bf93a034ff6974e29ed88a64f576
Tet Nay Lin <tetnaylin168@gmail.com>
2024-10-18 21:45:20 +1100

    Add animated background to Landing page

---
+++ frontend/src/components/animated-background/AnimatedBackground.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './animated-bg.module.css';
+
+const ListComponent = () => {
+
+  return (
+    <ul className={styles.AmiBg}>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+};
+
+export default ListComponent;
--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -4,26 +4,28 @@ import CssBaseline from '@mui/material/CssBaseline';
 import ProfileImage from '../../assets/logo.png';
 import { Avatar, Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import AnimatedBackground from "../animated-background/AnimatedBackground"
 
 export default function Landing() {
   const navigate = useNavigate();
   
   return (
       <><CssBaseline enableColorScheme />
+      <AnimatedBackground/>
         <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
           <Avatar
             alt="Profile Image"
             src={ProfileImage}
-            sx={{ width: 300, height: 300 }} />
+            sx={{ width: 250, height: 250 }} />
         </Box>
         <Typography
             component="h1"
             variant="h4"
-            sx={{ width: '100%', fontSize: 'clamp(2rem, 20vw, 5rem)', fontFamily:'cursive' }}
+            sx={{ width: '100%', fontSize: 'clamp(1.7rem, 17vw, 4.5rem)', fontFamily:'cursive', zIndex:'3', align: 'center' }}
           >
             PRESENT
           </Typography>
-        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 10, justifyContent: 'center', pt: '15%' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 7, justifyContent: 'center', pt: '15%' }}>
           <Button
             // type="submit"
             onClick={() => navigate("/login/attendee")}
